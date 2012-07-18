6760c5f49b616a6fbf60fed369f10f360156e30f
ryree0 <ryree0@gmail.com>
2012-07-17 21:45:13 -0400

    Zipalign compiled for ARM Copied zipalign from build/tools into external/armzipalign in order to run zipalign straight from the phone.

---
+++ Android.mk
@@ -0,0 +1,33 @@
+# 
+# Copyright 2008 The Android Open Source Project
+#
+# Zip alignment tool
+#
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	ZipAlign.cpp \
+	ZipEntry.cpp \
+	ZipFile.cpp
+
+LOCAL_C_INCLUDES += external/zlib
+
+LOCAL_SHARED_LIBRARIES := \
+	libutils \
+	libcutils \
+        libz
+
+LOCAL_C_INCLUDES := \
+        external/zlib
+
+ifeq ($(HOST_OS),linux)
+LOCAL_LDLIBS += -lrt
+endif
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := armzipalign
+
+include $(BUILD_EXECUTABLE)
+
